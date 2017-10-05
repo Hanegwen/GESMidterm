40b4f782421ca7be4913613e4e79c7febf5e04c6
Mike Morrone <Hanegwen725@gmail.com>
2017-10-04 22:16:26 -0500

    Just Tweaked stuff

--- 2DDemo/Assets/Scripts/CameraFollowPlayer.cs
+++ 2DDemo/Assets/Scripts/CameraFollowPlayer.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    GameObject player;
+
+    public float speed = 2.0f;
+
+    public Vector3 offset;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+
+    void Update()
+    {
+
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 position = this.transform.position;
+        position.y = Mathf.Lerp((this.transform.position.y - offset.y), player.transform.position.y, interpolation);
+        position.x = Mathf.Lerp((this.transform.position.x - offset.x), player.transform.position.x, interpolation);
+
+        this.transform.position = position;
+
+
+
+    }
+
 }
